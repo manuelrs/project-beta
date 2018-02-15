4a31898a198ba405bf14a64df21091e5af79487b
Manuel Rojas <mr.manuell@gmail.com>
2018-02-15 19:31:56 +0100

    Added materialize.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { ensureLoggedIn, ensureLoggedOut } = require("connect-ensure-login");
 
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", ensureLoggedOut("/dashboard"), (req, res, next) => {
+  res.render("index");
 });
 
 module.exports = router;
