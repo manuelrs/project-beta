0600b84804e330eb94e1f0643f2f0bce1970e28a
Manuel Rojas <mr.manuell@gmail.com>
2018-02-16 11:49:55 +0100

    Deploy to Heroku

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ const MongoStore = require("connect-mongo")(session);
 const flash = require("connect-flash");
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/project-beta");
+//mongoose.connect("mongodb://localhost/project-beta");
+mongoose.connect(process.env.MONGODB_URI);
 
 const app = express();
 
